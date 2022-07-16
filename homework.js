dfc97253fdca103973c607d21bf2dc920bbac45c
Olga <105517300+OlgaD-crypto@users.noreply.github.com>
2022-07-16 18:49:36 +0200

    Added forecast for current position

--- homework.js
+++ homework.js
@@ -17,6 +17,7 @@ todayDate.innerHTML = `${date.format(Date.now())}`;
 // searching for the city's data
 let apiKey = "7ed20b3871d9e4f3837ef60fa128bf28";
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
+let apiUrlForecast = "https://api.openweathermap.org/data/2.5/onecall?";
 
 function search(city) {
   axios
@@ -53,6 +54,11 @@ function currentPosition(position) {
   axios
     .get(`${apiUrl}lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
     .then(displayTemp);
+  axios
+    .get(
+      `${apiUrlForecast}lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=metric`
+    )
+    .then(displayForecast);
 }
 
 const rad = document.querySelectorAll('input[type="radio"]');
@@ -103,4 +109,34 @@ function displayTemp(response) {
 }
 
 getPosition();
-// search("Paris");
+
+function searchDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  forecastDates = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return forecastDates[day];
+}
+let newDay = document.querySelector("#forecast");
+
+function displayForecast(response) {
+  console.log(response);
+  let days = response.data.daily;
+  days.forEach(function (day, index) {
+    if (index < 5) {
+      let tempMax = Math.round(day.temp.max);
+      let tempMin = Math.round(day.temp.min);
+      newDay.innerHTML += `<div class="col">
+          <div id="day">${searchDay(day.dt)}</div>
+          <img id="icon-f" src="http://openweathermap.org/img/wn/${
+            day.weather[0].icon
+          }@2x.png" />
+          <div id="temp-f">${tempMax}° | ${tempMin}°</div>
+          <div id="wind-f">Wind: ${day.wind_speed} km/h </div>
+          </div>
+  `;
+    }
+
+    return newDay;
+  });
+  newDay += "</div>";
+}
